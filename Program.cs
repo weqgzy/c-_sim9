1239fe3e3b2db2702fe3c02217f15b0356b8e6e1
SAVAGE <proj_1>
2023-05-11 12:24:54 +0300

    commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int N = InputInt("введите число: ");
+
+PrintNum(N);
+
+void PrintNum(int N){
+	if(0 < N){
+		Console.Write(N + " ");
+        PrintNum(N - 1);
+		
+	}
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
